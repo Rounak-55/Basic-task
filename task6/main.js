c9632880e5b0cdcb9fa09bee9e16ee7615f26d0c
Arnab Biswas <khanp4397@gmail.com>
2024-01-17 21:20:09 +0530

    Added Task 6

---
+++ task6/main.js
@@ -0,0 +1,35 @@
+// let menu = ['fish', 'pizza', 'biryani']
+
+// function checkArrayOrNot(arr) {
+//     if (Array.isArray(arr)) {
+//         console.log("This is Array")
+//     } else {
+//         console.log("This is not Array")
+//     }
+// }
+
+// checkArrayOrNot(menu)
+// checkArrayOrNot("jhvygtfv")
+
+
+let studentName = ['rounak', 'arnab', 'debanshu'];
+
+// function cloneArray(arr) {
+//     return [...arr];
+// }
+
+// const newArray = cloneArray(studentName)
+
+// let fruits = ["Banana", "coconut", "pineapple", "Orange", "Apple", "Mango"];
+
+// function getElements(arr, n=1) {
+//     console.log(arr.slice(0, n))
+// }
+// getElements(fruits)
+// getElements(fruits, 3)
+
+const nums = [1, 6, 3, 3, 6, 8, 2, 1, 3, 567, 34, 9]
+
+for (let i = 0; i < nums.length; i++) {
+    console.log(nums[i])
+}
